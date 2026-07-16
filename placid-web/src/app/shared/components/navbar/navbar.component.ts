8ba6db4d1a9c98505901cf58e3cedba0fba3a570
Hansen <samhansen.dev@gmail.com>
2026-07-16 20:19:36 +0300

    feat: add navbar

---
+++ placid-web/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { ZardButtonComponent } from '@/shared/components/button';
+
+@Component({
+  selector: 'app-navbar-component',
+  templateUrl: 'navbar.html',
+})
+export class NavbarComponent {}
