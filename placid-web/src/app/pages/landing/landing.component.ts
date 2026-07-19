f2e24c2abf062639e9212336b536d51921f08091
Hansen <samhansen.dev@gmail.com>
2026-07-20 00:50:58 +0300

    fix: show navbar and footer only in landing page

--- placid-web/src/app/pages/landing/landing.component.ts
+++ placid-web/src/app/pages/landing/landing.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '@/shared/components/navbar/navbar.component';
+import { FooterComponent } from '@/shared/components/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent, FooterComponent],
   selector: 'app-landing-page',
   templateUrl: 'landing.html',
 })
