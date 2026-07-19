d743fb6d40f4c7c031dc4d0122084931daaa8898
Hansen <samhansen.dev@gmail.com>
2026-07-19 23:12:56 +0300

    fix: fix navbar padding

--- placid-web/src/app/shared/components/navbar/navbar.component.ts
+++ placid-web/src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ZardButtonComponent } from '@/shared/components/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-component',
   templateUrl: 'navbar.html',
+  imports: [RouterLink],
 })
 export class NavbarComponent {}
