a5bde835ac530a154adad60171a2412b27825cfa
Hansen <samhansen.dev@gmail.com>
2026-07-19 23:12:44 +0300

    feat: add home page

--- placid-web/src/app/pages/home/home.component.ts
+++ placid-web/src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import SoundScape from '@/shared/models/soundscape';
+import fetchSoundscapes from '@/shared/services/sounds';
 
 @Component({
   standalone: true,
