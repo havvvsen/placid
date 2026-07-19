66961d15f3dc40fe56874cffe4a0a6ead5e1998f
Hansen <samhansen.dev@gmail.com>
2026-07-19 18:53:16 +0300

    fix: remove leading / from path names

--- placid-web/src/app/app.routes.ts
+++ placid-web/src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home/home.component';
 import { LoginPageComponent } from './pages/login/login.component';
+import { RegisterPageComponent } from './pages/register/register.component';
+import { LandingPageComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
   {
-    path: '/',
-    component: HomePageComponent,
+    path: '',
+    component: LandingPageComponent,
   },
   {
-    path: '/login',
+    path: 'login',
     component: LoginPageComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
   },
 ];
