f7c3ac5ea16ffcc4a4822245f6d518f410885126
Hansen <samhansen.dev@gmail.com>
2026-07-20 00:53:02 +0300

    chore: add user model

--- spring-backend/src/main/java/com/placid/spring_backend/models/User.java
+++ spring-backend/src/main/java/com/placid/spring_backend/models/User.java
@@ -1,7 +1,11 @@
 package com.placid.spring_backend.models;
 
+import org.springframework.data.annotation.Id;
+
 public class User {
+    @Id
     private String id;
+
     private String email;
     private String password;
 }
