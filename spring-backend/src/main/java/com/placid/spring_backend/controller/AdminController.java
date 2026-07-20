ccb4140de10fd4955cfbcd0f31c399e02bc1871c
Hansen <samhansen.dev@gmail.com>
2026-07-21 01:26:05 +0300

    feat: add admin controller

--- spring-backend/src/main/java/com/placid/spring_backend/controller/AdminController.java
+++ spring-backend/src/main/java/com/placid/spring_backend/controller/AdminController.java
@@ -1,4 +1,4 @@
-package com.placid.spring_backend.controller;
+package com.placid.spring_backend.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
