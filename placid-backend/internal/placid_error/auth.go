748766f3b15464bc17180b1d887580ef7a9db9f5
Hansen <samhansen.dev@gmail.com>
2026-07-21 01:29:30 +0300

    chore: add custom errors

---
+++ placid-backend/internal/placid_error/auth.go
@@ -0,0 +1,9 @@
+package placiderror
+
+import "errors"
+
+var ErrInsecurePassword error = errors.New("Password cannot be less than 6 characters")
+var ErrInvalidEmail error = errors.New("Please provide a valid email")
+var ErrInvalidCredentials error = errors.New("Invalid credentials")
+var ErrBadRequest error = errors.New("Bad request")
+var ErrInternalServerError error = errors.New("Internal Server Error")
