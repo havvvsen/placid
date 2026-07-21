9114dfb45057cc3ab3348d68bb128a0497cb3ec3
Hansen <samhansen.dev@gmail.com>
2026-07-21 15:28:35 +0300

    fix: try now button routes to login pae

--- placid-web/src/app/pages/landing/landing.component.ts
+++ placid-web/src/app/pages/landing/landing.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '@/shared/components/navbar/navbar.component';
 import { FooterComponent } from '@/shared/components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent],
+  imports: [CommonModule, NavbarComponent, FooterComponent, RouterLink],
   selector: 'app-landing-page',
   templateUrl: 'landing.html',
 })
