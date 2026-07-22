626f9dbc52822613d21aceb72c78e86c8958c7bc
Hansen <samhansen.dev@gmail.com>
2026-07-22 14:15:56 +0300

    fix: rm unused imports

--- placid-web/src/app/app.ts
+++ placid-web/src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('placid-web');
+  protected readonly title = signal('placid');
 }
