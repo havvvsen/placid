8b3f33668b7c5f44480c4932071027f62caa2d47
Hansen <samhansen.dev@gmail.com>
2026-07-22 14:15:21 +0300

    feat: bootstrap http client

--- placid-web/src/app/app.config.ts
+++ placid-web/src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideZard } from '@/shared/core/provider/providezard';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideZard(),]
+    provideZard(),
+    provideHttpClient(),
+  ],
 };
