4370f2f759c6c174a8c5cf506bcaac96512a5b2f
Hansen <samhansen.dev@gmail.com>
2026-07-22 14:16:31 +0300

    feat: handle register

--- placid-web/src/app/pages/register/register.component.ts
+++ placid-web/src/app/pages/register/register.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import sanitizeCredentials from '@/shared/utils/sanitizer';
+
+interface RegisterResponse {
+  message: string;
+}
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   selector: 'app-register-page',
   templateUrl: 'register.html',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  private router = inject(Router);
+  private httpClient = inject(HttpClient);
+  email = '';
+  password = '';
+  loading = false;
+
+  onSubmit() {
+    this.loading = true;
+
+    try {
+      sanitizeCredentials(this.email, this.password);
+    } catch (e: any) {
+      alert(e);
+      return;
+    }
+
+    const body = {
+      email: this.email,
+      password: this.password,
+    };
+
+    console.log(body);
+
+    this.httpClient
+      .post<RegisterResponse>('http://localhost:3000/api/v1/auth/register', body, {
+        observe: 'response',
+      })
+      .subscribe({
+        next: (res) => {
+          this.loading = false;
+          this.router.navigateByUrl('/login');
+          alert(res.body?.message);
+        },
+        error: (err: HttpErrorResponse) => {
+          this.loading = false;
+          alert(err.error.error);
+        },
+      });
+  }
+}
