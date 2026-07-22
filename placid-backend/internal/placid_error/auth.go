f37933217f62c830d9a22a971d002b39fda255e6
Hansen <samhansen.dev@gmail.com>
2026-07-22 14:15:05 +0300

    chore: add custom errors

--- placid-backend/internal/placid_error/auth.go
+++ placid-backend/internal/placid_error/auth.go
@@ -7,3 +7,5 @@ var ErrInvalidEmail error = errors.New("Please provide a valid email")
 var ErrInvalidCredentials error = errors.New("Invalid credentials")
 var ErrBadRequest error = errors.New("Bad request")
 var ErrInternalServerError error = errors.New("Internal Server Error")
+
+var ErrEmailExistsNewsletter error = errors.New("Email already exists in the newsletter")
