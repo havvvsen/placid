9204b2c3e615ffcf5002bbd306303b8aff9dacfc
Hansen <samhansen.dev@gmail.com>
2026-07-23 12:15:56 +0300

    feat: load and render soundscapes

--- placid-web/src/app/pages/home/home.component.ts
+++ placid-web/src/app/pages/home/home.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import SoundScape from '@/shared/models/soundscape';
-import fetchSoundscapes from '@/shared/services/sounds';
+import { httpResource } from '@angular/common/http';
+import { PlayerComponent } from './components/player/player.component';
+
+interface SoundScape {
+  id: string;
+  name: string;
+  url: string;
+}
+enum Mood {
+  focus = 'focus',
+  relax = 'relax',
+  sleep = 'sleep',
+}
+interface SoundScapesResponse {
+  focus: SoundScape[];
+  relax: SoundScape[];
+  sleep: SoundScape[];
+}
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   selector: 'app-home-page',
   templateUrl: 'home.html',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  defaultSoundscape: SoundScape | null = null;
+
+  moods = Object.values(Mood).map((value, index) => ({
+    value,
+    index,
+  }));
+
+  productsResource = httpResource<SoundScapesResponse>(() => ({
+    url: 'http://localhost:3000/api/v1/soundscapes',
+  }));
+}
