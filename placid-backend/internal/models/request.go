12fb5f4aed2e9ddc089116e88b0bc998c1e44474
Hansen <samhansen.dev@gmail.com>
2026-07-25 17:55:32 +0300

    chore: add request models

---
+++ placid-backend/internal/models/request.go
@@ -0,0 +1,41 @@
+package models
+
+import "placid-backend/internal/database"
+
+type RegisterRequest struct {
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type UserRequest struct {
+	Email string `json:"email" validate:"required"`
+	Token string `json:"token" validate:"required"`
+}
+
+type DeleteUserRequest struct {
+	Email    string `json:"email" validate:"required"`
+	Token    string `json:"token" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+type AdminUploadTrackRequest struct {
+	Email string         `json:"email" validate:"required"`
+	Token string         `json:"token" validate:"required"`
+	Track database.Track `json:"track" validate:"required"`
+}
+
+type AdminDeleteTrackRequest struct {
+	Email string         `json:"email" validate:"required"`
+	Token string         `json:"token" validate:"required"`
+	Track database.Track `json:"track" validate:"required"`
+}
+type SubscribeToNewsletterRequest struct {
+	Email string `json:"email" validate:"required"`
+}
+type UnsubscribeFromNewsletterRequest struct {
+	Email string `json:"email" validate:"required"`
+}
