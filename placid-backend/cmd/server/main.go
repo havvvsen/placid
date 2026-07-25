35b774643167cd9d595c5d1182ac4a70df6c0840
Hansen <samhansen.dev@gmail.com>
2026-07-25 17:57:56 +0300

    chore: setup all endpoints

--- placid-backend/cmd/server/main.go
+++ placid-backend/cmd/server/main.go
@@ -2,306 +2,74 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"log"
 	"log/slog"
 	"os"
+	"placid-backend/internal/config"
 	"placid-backend/internal/database"
-	"placid-backend/internal/models"
-	placiderror "placid-backend/internal/placid_error"
-	"placid-backend/pkg/utils"
-	"strings"
+	"placid-backend/internal/handlers"
+	"placid-backend/internal/placid"
 
 	"github.com/gofiber/fiber/v3"
-	"github.com/gofiber/fiber/v3/middleware/cors"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/redis/go-redis/v9"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type Server struct {
-	app       *fiber.App
-	pgConn    *pgx.Conn
-	logger    *slog.Logger
-	rdbClient *redis.Client
-}
-
 func main() {
+	ctx := context.Background()
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	var err error
+
 	app := fiber.New()
 
-	app.Use(
-		cors.New(
-			cors.Config{
-				AllowOrigins: []string{"http://localhost:4200"},
-				AllowMethods: []string{"GET", "PUT", "POST", "DELETE", "OPTIONS"},
-				AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
-			},
-		),
-	)
+	server := &placid.Server{
+		Logger: logger,
+		Cfg: &placid.Config{
+			ApiConfig:      &placid.ApiConfig{},
+			RedisConfig:    &placid.RedisConfig{},
+			PostgresConfig: &placid.PostgresConfig{},
+		},
+	}
 
-	rdbClient := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
+	if server.Cfg, err = config.FillEnv(); err != nil {
+		server.Logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", server.Cfg.RedisConfig.RedisHost, server.Cfg.RedisConfig.RedisPort),
+		Username: server.Cfg.RedisConfig.RedisUsername,
+		Password: server.Cfg.RedisConfig.RedisPassword,
 		DB:       0,
 		Protocol: 2,
 	})
 
-	server := Server{
-		app:       app,
-		logger:    slog.New(slog.NewTextHandler(os.Stdout, nil)),
-		rdbClient: rdbClient,
+	server.RedisClient = redisClient
+
+	dbConn := config.ConnectPlacidDb(ctx, logger, server.Cfg.PostgresConfig)
+
+	server.Queries = database.New(dbConn)
+	server.App = app
+
+	config.ConfigureCors(server.App, server.Cfg.AllowedCORSOrigins)
+
+	// Register handlers
+	handlers.RegisterRegisterHandler(server)
+	handlers.RegisterLoginHandler(server)
+	handlers.RegisterTracksHandler(server)
+	handlers.RegisterUserHandler(server)
+	handlers.RegisterDeleteAccountHandler(server)
+	handlers.RegisterJoinNewsletterHandler(server)
+	handlers.RegisterAdminUploadTrackHandler(server)
+	handlers.RegisterAdminDeleteTracksHandler(server)
+
+	if err = app.Listen(
+		fmt.Sprintf(
+			"%s:%s",
+			server.Cfg.ApiConfig.ApiHost,
+			server.Cfg.ApiConfig.ApiPort,
+		)); err != nil {
+		server.Logger.Error(err.Error())
+
+		os.Exit(1)
 	}
-
-	pgConn, err := database.ConnectDb(server.logger)
-
-	if err != nil {
-		server.logger.Error(fmt.Sprintf("Failed to connect to database: %s\n", err.Error()))
-	}
-
-	server.pgConn = pgConn
-
-	app.Post("/api/v1/auth/register", func(c fiber.Ctx) error {
-		var user models.User
-		var pgErr *pgconn.PgError
-
-		if c.HasBody() {
-			if err := c.Bind().Body(&user); err != nil {
-				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
-			}
-
-			err := utils.SanitizeAuthRequest(&user)
-
-			if err != nil {
-				if errors.Is(err, placiderror.ErrInsecurePassword) || errors.Is(err, placiderror.ErrInvalidEmail) {
-					return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-				}
-
-				server.logger.Error(fmt.Sprintf("Failed to sanitize auth request: %s\n", err.Error()))
-				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
-			}
-
-			passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-
-			if err != nil {
-				server.logger.Error(fmt.Sprintf("Failed to hash password: %s\n", err.Error()))
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Registration failed"})
-			}
-
-			// Sign up user
-			_, err = server.pgConn.Exec(context.Background(), "INSERT INTO users ( email, password ) VALUES ( $1, $2 );", user.Email, passwordHash)
-
-			if err != nil {
-				// Duplicate key violation. User already exists
-				if errors.As(err, &pgErr) && pgErr.Code == "23505" {
-					return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": fmt.Sprintf("Email %s is already in use\n", user.Email)})
-				}
-
-				server.logger.Error(fmt.Sprintf("Failed to sign up user: %s\n", err.Error()))
-
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Registration failed"})
-
-			}
-
-			return c.JSON(fiber.Map{"message": "Registration successful"})
-		}
-
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Request has no body"})
-	})
-
-	app.Post("/api/v1/auth/login", func(c fiber.Ctx) error {
-		var user models.User
-		var inDatabaseHashedPassword []byte
-
-		if c.HasBody() {
-			if err := c.Bind().Body(&user); err != nil {
-				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
-			}
-
-			row := pgConn.QueryRow(context.Background(), "SELECT password FROM users WHERE email=$1;", user.Email)
-
-			if err := row.Scan(&inDatabaseHashedPassword); err != nil {
-				if errors.Is(err, pgx.ErrNoRows) {
-					// User does not exist in database
-					return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": placiderror.ErrInvalidCredentials.Error()})
-				}
-
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": placiderror.ErrInternalServerError.Error()})
-			}
-
-			err = bcrypt.CompareHashAndPassword(inDatabaseHashedPassword, []byte(user.Password))
-
-			if err != nil {
-				if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
-					return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": placiderror.ErrInvalidCredentials.Error()})
-				}
-
-				server.logger.Error(fmt.Sprintf("Compare hash and pass: %s\n", err.Error()))
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": placiderror.ErrInternalServerError.Error()})
-			}
-
-			return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Authentication successful", "token": "jwt-token-8762y8a82n0824bsbfy82"})
-		}
-
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
-	})
-
-	app.Delete("/api/v1/delete-account", func(c fiber.Ctx) error {
-		var user models.User
-		var inDatabaseHashedPassword []byte
-
-		if c.HasBody() {
-			if err := c.Bind().Body(&user); err != nil {
-				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
-			}
-
-			rows, err := pgConn.Query(context.Background(), "SELECT password FROM users WHERE email==$1;", user.Email)
-
-			if errors.Is(err, pgx.ErrNoRows) {
-				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": placiderror.ErrInvalidCredentials.Error()})
-			}
-
-			if err := rows.Scan(&inDatabaseHashedPassword); err != nil {
-				server.logger.Error(fmt.Sprintf("Failed to scan password hash: %s\n", err.Error()))
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": placiderror.ErrInternalServerError.Error()})
-			}
-
-			err = bcrypt.CompareHashAndPassword(inDatabaseHashedPassword, []byte(user.Password))
-
-			if err != nil {
-				if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
-					return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": placiderror.ErrInvalidCredentials.Error()})
-				}
-
-				server.logger.Error(fmt.Sprintf("Failed to compare hash and password: %s\n", err.Error()))
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": placiderror.ErrInternalServerError.Error()})
-			}
-
-			_, err = pgConn.Exec(context.Background(), "DELETE FROM users WHERE email = $1;", user.Email)
-
-			if err != nil {
-				server.logger.Error(fmt.Sprintf("Failed to delete account: %s\n", err.Error()))
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to delete account"})
-
-			}
-
-			c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Account deleted successfully"})
-		}
-
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
-
-	})
-
-	app.Post("/api/v1/admin/upload", func(c fiber.Ctx) error {
-
-		return c.SendString("H")
-	})
-
-	app.Get("/api/v1/soundscapes", func(c fiber.Ctx) error {
-		soundscapes := fiber.Map{
-			"focus": []models.Soundscape{
-				{
-					Id:       "1",
-					Mood:     "focus",
-					Name:     "Rain Noise",
-					AudioUrl: "https://placid-soundscapes-s3-bucket.s3.us-east-1.amazonaws.com/relax/4b.mp3",
-				},
-				{
-					Id:       "2",
-					Mood:     "focus",
-					Name:     "Click Sounds",
-					AudioUrl: "https://placid-soundscapes-s3-bucket.s3.us-east-1.amazonaws.com/relax/4b.mp3",
-				},
-				{
-					Id:       "3",
-					Mood:     "focus",
-					Name:     "ASMR sounds",
-					AudioUrl: "https://placid-soundscapes-s3-bucket.s3.us-east-1.amazonaws.com/relax/4b.mp3",
-				},
-			},
-			"relax": []models.Soundscape{
-				{
-					Id:       "4",
-					Mood:     "relax",
-					Name:     "Pinch sounds",
-					AudioUrl: "https://placid-soundscapes-s3-bucket.s3.us-east-1.amazonaws.com/relax/4b.mp3",
-				},
-				{
-					Id:       "5",
-					Mood:     "relax",
-					Name:     "Bay Sounds",
-					AudioUrl: "https://placid-soundscapes-s3-bucket.s3.us-east-1.amazonaws.com/relax/4b.mp3",
-				},
-				{
-					Id:       "6",
-					Mood:     "relax",
-					Name:     "Ocean Waves",
-					AudioUrl: "https://placid-soundscapes-s3-bucket.s3.us-east-1.amazonaws.com/relax/4b.mp3",
-				},
-			},
-			"sleep": []models.Soundscape{
-				{
-					Id:       "7",
-					Mood:     "sleep",
-					Name:     "Baby sleep",
-					AudioUrl: "https://placid-soundscapes-s3-bucket.s3.us-east-1.amazonaws.com/relax/4b.mp3",
-				},
-				{
-					Id:       "8",
-					Mood:     "sleep",
-					Name:     "Click Sounds",
-					AudioUrl: "https://placid-soundscapes-s3-bucket.s3.us-east-1.amazonaws.com/relax/4b.mp3",
-				},
-				{
-					Id:       "9",
-					Mood:     "sleep",
-					Name:     "ASMR sounds",
-					AudioUrl: "https://placid-soundscapes-s3-bucket.s3.us-east-1.amazonaws.com/relax/4b.mp3",
-				},
-			},
-		}
-
-		return c.Status(fiber.StatusOK).JSON(soundscapes)
-	})
-
-	app.Delete("/api/v1/admin/delete-sound", func(c fiber.Ctx) error {
-		return c.SendString("H")
-	})
-
-	app.Post("/api/v1/join-newsletter", func(c fiber.Ctx) error {
-		var request models.BasicEmailModel
-		var disposable models.BasicEmailModel
-		if c.HasBody() {
-			if err := c.Bind().Body(&request); err != nil {
-				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
-			}
-
-			row := pgConn.QueryRow(context.Background(), "SELECT email FROM newsletter WHERE email = $1;", strings.Trim(request.Email, " "))
-			err := row.Scan(&disposable.Email)
-
-			if err != nil {
-				if errors.Is(err, pgx.ErrNoRows) {
-					query := "INSERT INTO newsletter (email) VALUES ( $1 );"
-					_, err = pgConn.Exec(context.Background(), query, request.Email)
-
-					if err != nil {
-						server.logger.Error(fmt.Sprintf("Failed to add email to newsletter: %s\n", err.Error()))
-						return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to add email to newsletter"})
-					}
-
-					return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Successful added to newsletter"})
-				}
-
-				server.logger.Error(fmt.Sprintf("Failed to add email to newsletter: %s\n", err.Error()))
-				return c.Status(fiber.StatusConflict).JSON(fiber.Map{"message": placiderror.ErrInternalServerError.Error()})
-			}
-
-			return c.Status(fiber.StatusConflict).JSON(fiber.Map{"error": placiderror.ErrEmailExistsNewsletter.Error()})
-		}
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Bad Request"})
-	})
-
-	log.Fatal(app.Listen(":3000"))
-
 }
