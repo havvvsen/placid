6188a56d73978143812724666979aec199d8e4d1
Hansen <samhansen.dev@gmail.com>
2026-07-25 17:55:05 +0300

    feat: add newsletter route

---
+++ placid-backend/internal/handlers/newsletter.go
@@ -0,0 +1,85 @@
+package handlers
+
+import (
+	"errors"
+	"fmt"
+	"placid-backend/internal/models"
+	"placid-backend/internal/placid"
+	placiderror "placid-backend/internal/placid_error"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/jackc/pgx/v5"
+)
+
+func RegisterJoinNewsletterHandler(server *placid.Server) {
+	server.App.Post(server.Cfg.Endpoints.AddNewsletterSubscriber, func(c fiber.Ctx) error {
+		ctx := c.Context()
+		var request models.SubscribeToNewsletterRequest
+
+		if c.HasBody() {
+			if err := c.Bind().Body(&request); err != nil {
+				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
+			}
+
+			_, err := server.Queries.GetNewsletterSubscriber(ctx, request.Email)
+
+			if err != nil {
+				if errors.Is(err, pgx.ErrNoRows) {
+					err := server.Queries.AddNewsletterSubsciber(ctx, request.Email)
+
+					if err != nil {
+						server.Logger.Error(fmt.Sprintf("Failed to add email to newsletter: %s", err.Error()))
+						return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": placiderror.ErrInternalServerError.Error()})
+					}
+
+					return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "Successful added to newsletter"})
+				}
+
+				server.Logger.Error(fmt.Sprintf("Failed to add email to newsletter: %s", err.Error()))
+				return c.Status(fiber.StatusConflict).JSON(fiber.Map{"message": placiderror.ErrInternalServerError.Error()})
+			}
+
+			return c.Status(fiber.StatusConflict).JSON(fiber.Map{"error": placiderror.ErrEmailExistsNewsletter.Error()})
+		}
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
+	})
+
+	server.Logger.Info(fmt.Sprintf("Registered %s", server.Cfg.Endpoints.AddNewsletterSubscriber))
+}
+
+func RegisterDeleteNewsletterSubscriberHandler(server *placid.Server) {
+	server.App.Delete(server.Cfg.Endpoints.DeleteNewsletterSubscriber, func(c fiber.Ctx) error {
+		ctx := c.Context()
+
+		var request models.UnsubscribeFromNewsletterRequest
+
+		if c.HasBody() {
+			if err := c.Bind().Body(&request); err != nil {
+				return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
+			}
+
+			_, err := server.Queries.GetNewsletterSubscriber(ctx, request.Email)
+
+			if err != nil {
+				if errors.Is(err, pgx.ErrNoRows) {
+					return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "You are currently not subscribed to our newsletter. Please consider subscribing"})
+				}
+
+				server.Logger.Error(fmt.Sprintf("Failed to remove email from newsletter: %s", err.Error()))
+				return c.Status(fiber.StatusConflict).JSON(fiber.Map{"message": placiderror.ErrInternalServerError.Error()})
+			}
+
+			err = server.Queries.DeleteNewsletterSubscriber(ctx, request.Email)
+
+			if err != nil {
+				server.Logger.Error(fmt.Sprintf("Failed to remove email from newsletter: %s", err.Error()))
+				return c.Status(fiber.StatusConflict).JSON(fiber.Map{"message": placiderror.ErrInternalServerError.Error()})
+			}
+
+			return c.Status(fiber.StatusOK).JSON(fiber.Map{"error": "You have successfully unsubscribed from our newsletter"})
+		}
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": placiderror.ErrBadRequest.Error()})
+
+	})
+	server.Logger.Info(fmt.Sprintf("Registered %s", server.Cfg.Endpoints.AddNewsletterSubscriber))
+}
