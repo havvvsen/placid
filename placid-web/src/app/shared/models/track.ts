56595713525d2cf768c1d6b695ab17f51c0979ee
Hansen <samhansen.dev@gmail.com>
2026-07-26 13:14:12 +0300

    refactor: rename soundscape to track

---
+++ placid-web/src/app/shared/models/track.ts
@@ -0,0 +1,17 @@
+import { Mood } from './mood';
+
+export default class Track {
+  id: number;
+  name: string;
+  mood: string;
+  bgUrl: string;
+  audioUrl: string;
+
+  constructor(id: number, name: string, bgUrl: string, mood: string, audioUrl: string) {
+    this.id = id;
+    this.name = name;
+    this.mood = mood;
+    this.audioUrl = audioUrl;
+    this.bgUrl = bgUrl;
+  }
+}
