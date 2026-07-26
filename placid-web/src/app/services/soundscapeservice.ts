9a730b9fa45b536a6d2969295de8c573ce0fbda7
Hansen <samhansen.dev@gmail.com>
2026-07-26 13:13:46 +0300

    refactor: rename soundscape to track

--- placid-web/src/app/services/soundscapeservice.ts
+++
@@ -1,7 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Service } from '@angular/core';
-
-@Service()
-export class SoundscapeService {
-  private http = inject(HttpClient);
-}
