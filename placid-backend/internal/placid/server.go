f1af0bdc768c9fad39a8b2d1194993e94e7428f5
Hansen <samhansen.dev@gmail.com>
2026-07-26 12:38:29 +0300

    refactor: rm redis

--- placid-backend/internal/placid/server.go
+++ placid-backend/internal/placid/server.go
@@ -6,14 +6,12 @@ import (
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/jackc/pgx/v5"
-	"github.com/redis/go-redis/v9"
 )
 
 type Server struct {
-	Cfg         *Config
-	App         *fiber.App
-	DbConn      *pgx.Conn
-	Queries     *database.Queries
-	Logger      *slog.Logger
-	RedisClient *redis.Client
+	Cfg     *Config
+	App     *fiber.App
+	DbConn  *pgx.Conn
+	Queries *database.Queries
+	Logger  *slog.Logger
 }
