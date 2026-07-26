7f444ccea3d4db5c2be69216fa6c0554b332f8b0
Hansen <samhansen.dev@gmail.com>
2026-07-27 02:40:48 +0300

    refactor: minor changes

--- placid-web/src/app/pages/landing/landing.component.ts
+++ placid-web/src/app/pages/landing/landing.component.ts
@@ -10,14 +10,25 @@ import { Router, RouterLink } from '@angular/router';
   selector: 'app-landing-page',
   templateUrl: 'landing.html',
 })
-export class LandingPageComponent implements OnInit {
-  private router = inject(Router);
-
-  ngOnInit() {
-    let token = localStorage.getItem('token');
-
-    if (token) {
-      this.router.navigateByUrl('/home');
-    }
-  }
+export class LandingPageComponent {
 }
+// export class LandingPageComponent implements OnInit {
+//   private router = inject(Router);
+//
+//   ngOnInit() {
+//     const token = localStorage.getItem('token')?.trim();
+//
+//     console.log("Init ")
+//     if (token == "" || token == null || token == undefined) {
+//       return
+//     }
+//
+//     if (token != "" || token != null || token != undefined) {
+//       console.log("Is not null ")
+//       console.log(`Token: ${token}`)
+//       this.router.navigateByUrl('/home');
+//       return
+//     }
+//     console.log("Is null ")
+//   }
+// }
