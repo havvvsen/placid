59dabf8e4ab8340c442080465476560a2868df3f
Hansen <samhansen.dev@gmail.com>
2026-07-27 02:39:07 +0300

    feat: add track service

--- placid-web/src/app/services/trackservice.ts
+++ placid-web/src/app/services/trackservice.ts
@@ -1,107 +1,24 @@
-import { Mood } from '@/shared/models/mood';
-import Track from '@/shared/models/track';
-import { HttpClient } from '@angular/common/http';
-import { inject, Service } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Environment } from "../shared/constants/environment";
+import { TracksResponse } from '@/shared/models/response';
+import { Track } from '@/shared/models/track';
+import { Observable } from 'rxjs';
 
-export class TrackList {
-  focus: Track[] = [];
-  relax: Track[] = [];
-  sleep: Track[] = [];
-}
-
-@Service({
-  autoProvided: true,
+@Injectable({
+  providedIn: 'root',
 })
 export class TrackService {
   private http = inject(HttpClient);
-  private trackList: TrackList | null;
 
-  constructor() {
-    this.trackList = this.fetchTrackList();
-  }
+  public getTrackList(): Observable<Track[]> {
+    const token = localStorage.getItem('token');
+    let headers = new HttpHeaders();
+    if (token) {
+      headers = headers.set('Authorization', `Bearer ${token.trim()}`);
+    }
 
-  public getTrackList(): TrackList | null {
-    return this.trackList;
-  }
-
-  public fetchTrackList(): TrackList {
-    // TODO: add fetch logic
-    let tracks: Track[] = [
-      {
-        id: 1,
-        name: 'Summer Tides',
-        mood: 'focus',
-        audioUrl: 'http://localhost:8091/audio/stream',
-        bgUrl: 'https://imgur/img.png',
-      },
-      {
-        id: 2,
-        name: 'Weifer Heifer',
-        mood: 'anxiety',
-        audioUrl: 'http://localhost:8091/audio/stream',
-        bgUrl: 'https://imgur/img.png',
-      },
-      {
-        id: 3,
-        name: 'Kneegor Meegro',
-        mood: 'relax',
-        audioUrl: 'http://localhost:8091/audio/stream',
-        bgUrl: 'https://imgur/img.png',
-      },
-      {
-        id: 4,
-        name: 'CaseY Jones',
-        mood: 'sleep',
-        audioUrl: 'http://localhost:8091/audio/stream',
-        bgUrl: 'https://imgur/img.png',
-      },
-      {
-        id: 5,
-        name: 'CaseY Jones',
-        mood: 'focus',
-        audioUrl: 'http://localhost:8091/audio/stream',
-        bgUrl: 'https://imgur/img.png',
-      },
-      {
-        id: 6,
-        name: 'CaseY Jones',
-        mood: 'focus',
-        audioUrl: 'http://localhost:8091/audio/stream',
-        bgUrl: 'https://imgur/img.png',
-      },
-      {
-        id: 7,
-        name: 'CaseY Jones',
-        mood: 'focus',
-        audioUrl: 'http://localhost:8091/audio/stream',
-        bgUrl: 'https://imgur/img.png',
-      },
-      {
-        id: 8,
-        name: 'CaseY Jones',
-        mood: 'focus',
-        audioUrl: 'http://localhost:8091/audio/stream',
-        bgUrl: 'https://imgur/img.png',
-      },
-    ];
-
-    let trackList: TrackList = new TrackList();
-
-    tracks.map((track, _) => {
-      switch (track.mood) {
-        case 'relax':
-          trackList.relax.push(track);
-          break;
-        case 'focus':
-          trackList.focus.push(track);
-          break;
-        case 'sleep':
-          trackList.sleep.push(track);
-          break;
-      }
-    });
-
-    console.log(trackList);
-    return trackList;
+    return this.http.get<Track[]>(`${Environment.apiBaseUrl}/${Environment.endpoints.tracks}`, { headers });
   }
 }
+
