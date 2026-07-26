579c9892ccca496674167db0a9867bfc458ad42b
Hansen <samhansen.dev@gmail.com>
2026-07-26 14:48:47 +0300

    fix: implement proper logging for assets server

--- file-server/main.go
+++ file-server/main.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"log/slog"
 	"net/http"
 	"os"
 )
 
 func main() {
-	basePath := "/home/hansen/Projects/placid/tracks-server/assets"
+	basePath := "/home/ubuntu/placid/file-server/assets"
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	port, isSet := os.LookupEnv("FILE_SERVER_PORT")
+
+	if !isSet {
+		logger.Error("$FILE_SERVER_PORT is required")
+		os.Exit(1)
+	}
+
+	addr := fmt.Sprintf(":%s", port)
+
+	logger.Info(fmt.Sprintf("Serving assets at %s", addr))
+
 	server := http.FileServer(http.Dir(basePath))
 
-	if err := http.ListenAndServe(":3031", server); err != nil {
-		logger.Info("Server serving at :3031")
-		fmt.Println("Here")
-		log.Fatal(err.Error())
+	if err := http.ListenAndServe(addr, server); err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
 }
