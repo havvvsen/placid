8596d2226ce5b0b7028cbe3f95f5c046829c4c38
Hansen <samhansen.dev@gmail.com>
2026-07-27 02:41:25 +0300

    feat: add loading component

---
+++ placid-web/src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-loading-component",
+  templateUrl: "loading.html",
+  styleUrls: ["loading.css"]
+})
+export class LoadingComponent {
+
+}
