f33c53a9ceba5da4e47edf390acdfcdebece57cd
Hansen <samhansen.dev@gmail.com>
2026-07-27 00:01:22 +0300

    fix: use single quotes for values in sql init file

--- placid-backend/data/init.sql
+++ placid-backend/data/init.sql
@@ -1,56 +1,30 @@
 -- Focus
 INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("White noise", "focus", "focus/white_noise.mp3", "white_noise.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Heavenly vibraphone", "focus", "focus/heavenly_vibraphone.mp3", "heavenly_vibraphone.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Tibetan choir", "focus", "focus/tibetan_choir.mp3", "tibetan_choir.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Underwater", "focus", "focus/underwater.mp3", "underwater.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Nature adventure", "focus", "focus/nature_adventure.mp3", "nature_adventure.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Autumn walk", "focus", "focus/autumn_walk.mp3", "autumn_walk.jpg");
+VALUES
+    ('White noise', 'focus', 'focus/white_noise.mp3', 'white_noise.jpg'),
+    ('Heavenly vibraphone', 'focus', 'focus/heavenly_vibraphone.mp3', 'heavenly_vibraphone.jpg'),
+    ('Tibetan choir', 'focus', 'focus/tibetan_choir.mp3', 'tibetan_choir.jpg'),
+    ('Underwater', 'focus', 'focus/underwater.mp3', 'underwater.jpg'),
+    ('Nature adventure', 'focus', 'focus/nature_adventure.mp3', 'nature_adventure.jpg'),
+    ('Autumn walk', 'focus', 'focus/autumn_walk.mp3', 'autumn_walk.jpg');
 
 -- Relax
 INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Resonance bells", "relax", "relax/resonance_bells.mp3", "resonance_bells.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Temple bells", "relax", "relax/temple_bells.mp3", "temple_bells.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Japanese garden", "relax", "relax/japanese_garden.mp3", "japanese_garden.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Brown", "relax", "relax/brown_noise.mp3", "brown_noise.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Binaural beats", "relax", "relax/binaural_beats.mp3", "binaural_beats.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Piano notes", "relax", "relax/piano_notes.mp3", "piano_notes.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Beach vibes", "relax", "relax/beach_vibes.mp3", "beach_vibes.jpg");
+VALUES
+    ('Resonance bells', 'relax', 'relax/resonance_bells.mp3', 'resonance_bells.jpg'),
+    ('Temple bells', 'relax', 'relax/temple_bells.mp3', 'temple_bells.jpg'),
+    ('Japanese garden', 'relax', 'relax/japanese_garden.mp3', 'japanese_garden.jpg'),
+    ('Brown', 'relax', 'relax/brown_noise.mp3', 'brown_noise.jpg'),
+    ('Binaural beats', 'relax', 'relax/binaural_beats.mp3', 'binaural_beats.jpg'),
+    ('Piano notes', 'relax', 'relax/piano_notes.mp3', 'piano_notes.jpg'),
+    ('Beach vibes', 'relax', 'relax/beach_vibes.mp3', 'beach_vibes.jpg');
 
 -- Sleep
 INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Gray noise", "sleep", "sleep/gray_noise.mp3", "gray_noise.jpg");
+VALUES
+    ('Gray noise', 'sleep', 'sleep/gray_noise.mp3', 'gray_noise.jpg'),
+    ('Silver drift', 'sleep', 'sleep/silver_drift.mp3', 'silver_drift.jpg'),
+    ('Rain noise', 'sleep', 'sleep/rain_noise.mp3', 'rain_noise.jpg'),
+    ('Rain on tent', 'sleep', 'sleep/rain_on_tent.mp3', 'rain_on_tent.jpg'),
+    ('Stormy night', 'sleep', 'sleep/stormy_night.mp3', 'stormy_night.jpg');
 
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Silver drift", "sleep", "sleep/silver_drift.mp3", "silver_drift.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Rain noise", "sleep", "sleep/rain_noise.mp3", "rain_noise.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Rain on tent", "sleep", "sleep/rain_on_tent.mp3", "rain_on_tent.jpg");
-
-INSERT INTO tracks (name, mood, audio_url, bg_url)
-    VALUES ("Stormy night", "sleep", "sleep/stormy_night.mp3", "stormy_night.jpg");
