4541e47f4dd52a11247583dbef5de94e9973a1f8
Hansen <samhansen.dev@gmail.com>
2026-07-27 02:38:15 +0300

    feat: add auth service

---
+++ placid-web/src/app/services/authservice.ts
@@ -0,0 +1,56 @@
+import { Environment } from "@/shared/constants/environment";
+import { HttpClient } from "@angular/common/http"
+import { inject, Service } from "@angular/core"
+import { RegisterResponse, LoginResponse } from "@/shared/models/response";
+import InvalidInputError from "@/shared/exceptions/invalid_input";
+import PlayerService from "./playerservice";
+import { Router } from "@angular/router";
+
+@Service({
+  autoProvided: true
+})
+export default class AuthService {
+  private http = inject(HttpClient)
+  private router = inject(Router)
+  playerService = inject(PlayerService)
+
+  registerUser(email: string, password: string) {
+    let body = {
+      email: email,
+      password: password,
+    };
+
+    return this.http.post<RegisterResponse>(`${Environment.apiBaseUrl}/${Environment.endpoints.register}`, body, {
+      observe: 'response',
+    });
+  }
+
+  loginUser(email: string, password: string) {
+    let body = {
+      email: email,
+      password: password,
+    };
+
+    return this.http.post<LoginResponse>(`${Environment.apiBaseUrl}/${Environment.endpoints.login}`, body, { observe: 'response' });
+  }
+  logoutUser() {
+    if (this.playerService.isPlaying()) {
+      this.playerService.togglePlayStatus()
+    }
+
+    localStorage.removeItem("token")
+    this.router.navigateByUrl("/login")
+  }
+
+
+  sanitizeCredentials(email: string, password: string) {
+    if (!email.includes('@') || !email.includes('.') || email.length < 5) {
+      throw new InvalidInputError('Please provide a valid email');
+    }
+
+    if (password.length < 6) {
+      throw new InvalidInputError('Password cannot be less than 6 characters');
+    }
+  }
+
+}
