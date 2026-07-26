44b731725e6dd008fb7ec5175cec0687d93f49c2
Hansen <samhansen.dev@gmail.com>
2026-07-27 02:39:14 +0300

    feat: add user service

--- placid-web/src/app/services/userservice.ts
+++ placid-web/src/app/services/userservice.ts
@@ -1,5 +1,5 @@
-import Endpoints from '@/shared/constants/endpoints';
-import { HttpClient } from '@angular/common/http';
+import { Environment } from '@/shared/constants/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 
 @Service({
@@ -7,26 +7,32 @@ import { inject, Service } from '@angular/core';
 })
 class UserService {
   private http = inject(HttpClient);
-  private endPoints = new Endpoints();
+  private token
 
-  public registerUser(username: string, password: string) {
-    let body = {
-      username: username,
-      password: password,
-    };
-
-    this.http.post(this.endPoints.register, body, {
-      observe: 'response',
-    });
+  constructor() {
+    let token = localStorage.getItem("token")
+    this.token = token
   }
 
-  public loginUser(username: string, password: string) {
-    let body = {
-      username: username,
-      password: password,
-    };
+  public getUser(email: string) {
+    const headers = new HttpHeaders().set("Authorization", `Bearer ${this.token?.toString()}`)
 
-    this.http.post(this.endPoints.login, body, { observe: 'response' });
+    let body = {
+      email: email
+    }
+
+    return this.http.post(Environment.endpoints.user, body, { headers: headers, observe: 'response' });
+  }
+
+  public deleteAccount(email: string, password: string) {
+    const headers = new HttpHeaders().set("Authorization", `Bearer ${this.token?.toString()}`)
+
+    let body = {
+      email: email,
+      password: password
+    }
+
+    return this.http.post(Environment.endpoints.deleteAccount, body, { headers: headers, observe: 'response' });
   }
 }
 
