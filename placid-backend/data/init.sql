43c379485b3e9ee853f37e028f46d4a8b0eae55f
Hansen <samhansen.dev@gmail.com>
2026-07-26 23:48:01 +0300

    chore: add sql init script for tracks

---
+++ placid-backend/data/init.sql
@@ -0,0 +1,56 @@
+-- Focus
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("White noise", "focus", "focus/white_noise.mp3", "white_noise.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Heavenly vibraphone", "focus", "focus/heavenly_vibraphone.mp3", "heavenly_vibraphone.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Tibetan choir", "focus", "focus/tibetan_choir.mp3", "tibetan_choir.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Underwater", "focus", "focus/underwater.mp3", "underwater.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Nature adventure", "focus", "focus/nature_adventure.mp3", "nature_adventure.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Autumn walk", "focus", "focus/autumn_walk.mp3", "autumn_walk.jpg");
+
+-- Relax
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Resonance bells", "relax", "relax/resonance_bells.mp3", "resonance_bells.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Temple bells", "relax", "relax/temple_bells.mp3", "temple_bells.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Japanese garden", "relax", "relax/japanese_garden.mp3", "japanese_garden.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Brown", "relax", "relax/brown_noise.mp3", "brown_noise.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Binaural beats", "relax", "relax/binaural_beats.mp3", "binaural_beats.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Piano notes", "relax", "relax/piano_notes.mp3", "piano_notes.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Beach vibes", "relax", "relax/beach_vibes.mp3", "beach_vibes.jpg");
+
+-- Sleep
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Gray noise", "sleep", "sleep/gray_noise.mp3", "gray_noise.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Silver drift", "sleep", "sleep/silver_drift.mp3", "silver_drift.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Rain noise", "sleep", "sleep/rain_noise.mp3", "rain_noise.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Rain on tent", "sleep", "sleep/rain_on_tent.mp3", "rain_on_tent.jpg");
+
+INSERT INTO tracks (name, mood, audio_url, bg_url)
+    VALUES ("Stormy night", "sleep", "sleep/stormy_night.mp3", "stormy_night.jpg");
