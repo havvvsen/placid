c878947d5f9400a3f22a45312c376fcc27c0ff0c
Hansen <samhansen.dev@gmail.com>
2026-07-26 13:13:31 +0300

    refactor: rename soundscape to track

--- placid-web/src/app/pages/home/home.component.ts
+++ placid-web/src/app/pages/home/home.component.ts
@@ -1,23 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { httpResource } from '@angular/common/http';
 import { PlayerComponent } from './components/player/player.component';
+import { TrackList, TrackService } from '@/services/trackservice';
+import Track from '@/shared/models/track';
 
-interface SoundScape {
-  id: string;
-  name: string;
-  url: string;
-}
-enum Mood {
-  focus = 'focus',
-  relax = 'relax',
-  sleep = 'sleep',
-}
-interface SoundScapesResponse {
-  focus: SoundScape[];
-  relax: SoundScape[];
-  sleep: SoundScape[];
-}
+// interface TrackResponse {
+//   focus: Track[];
+//   relax: Track[];
+//   sleep: Track[];
+// }
 
 @Component({
   standalone: true,
@@ -26,14 +18,20 @@ interface SoundScapesResponse {
   templateUrl: 'home.html',
 })
 export class HomePageComponent {
-  defaultSoundscape: SoundScape | null = null;
+  soundscapeService = inject(TrackService);
+  trackList: TrackList | null;
+  defaultTrack: Track | undefined = undefined;
 
-  moods = Object.values(Mood).map((value, index) => ({
-    value,
-    index,
-  }));
+  // productsResource = httpResource<SoundScapesResponse>(() => ({
+  //   url: 'http://localhost:3000/api/v1/soundscapes',
+  // }));
 
-  productsResource = httpResource<SoundScapesResponse>(() => ({
-    url: 'http://localhost:3000/api/v1/soundscapes',
-  }));
+  constructor() {
+    this.trackList = this.soundscapeService.getTrackList();
+    this.defaultTrack = this.trackList?.focus?.[0];
+  }
+
+  printTrackList() {
+    console.log(this.trackList);
+  }
 }
