fd3016ab07e268a99e1602b1cb3e4b6dc52dd3d4
Hansen <samhansen.dev@gmail.com>
2026-07-26 12:37:52 +0300

    fix: rm token from request bodies

--- placid-backend/internal/models/request.go
+++ placid-backend/internal/models/request.go
@@ -14,23 +14,19 @@ type LoginRequest struct {
 
 type UserRequest struct {
 	Email string `json:"email" validate:"required"`
-	Token string `json:"token" validate:"required"`
 }
 
 type DeleteUserRequest struct {
 	Email    string `json:"email" validate:"required"`
-	Token    string `json:"token" validate:"required"`
 	Password string `json:"password" validate:"required"`
 }
 type AdminUploadTrackRequest struct {
 	Email string         `json:"email" validate:"required"`
-	Token string         `json:"token" validate:"required"`
 	Track database.Track `json:"track" validate:"required"`
 }
 
 type AdminDeleteTrackRequest struct {
 	Email string         `json:"email" validate:"required"`
-	Token string         `json:"token" validate:"required"`
 	Track database.Track `json:"track" validate:"required"`
 }
 type SubscribeToNewsletterRequest struct {
