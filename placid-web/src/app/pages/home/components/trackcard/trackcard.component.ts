f8b152d999a66f4fd3131235e29da92b5504c326
Hansen <samhansen.dev@gmail.com>
2026-07-27 02:40:06 +0300

    refactor: minor changes

--- placid-web/src/app/pages/home/components/trackcard/trackcard.component.ts
+++ placid-web/src/app/pages/home/components/trackcard/trackcard.component.ts
@@ -1,14 +1,28 @@
-import Track from '@/shared/models/track';
-import { Component, Input } from '@angular/core';
+import { Track } from '@/shared/models/track';
+import { Component, Input, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlayerService } from '@/services/playerservice';
 
 @Component({
   selector: 'home-track-card-component',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: 'trackcard.html',
 })
-export default class TrackCardComponent {
-  @Input() track: Track;
+export class TrackCardComponent {
+  @Input() track!: Track;
+  public playerService = inject(PlayerService);
 
-  constructor(track: Track) {
-    this.track = track;
+  get isPlaying(): boolean {
+    return this.playerService.currentTrack()?.id === this.track?.id && this.playerService.isPlaying();
+  }
+
+  playTrack() {
+    if (this.track) {
+      this.playerService.playTrack(this.track);
+    }
   }
 }
+
+export default TrackCardComponent;
+
