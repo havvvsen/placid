8e15661fa49e59c1622f6e52c73f49c1ae9a972d
Hansen <samhansen.dev@gmail.com>
2026-07-28 17:19:31 +0300

    feat: implement loading service

--- placid-web/src/app/pages/login/login.component.ts
+++ placid-web/src/app/pages/login/login.component.ts
@@ -4,6 +4,9 @@ import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import AuthService from '@/services/authservice';
+import { LoadingService } from '@/services/loadingservice';
+import { NotificationService } from '@/services/notificationservice';
+import InvalidInputError from '@/shared/exceptions/invalid_input';
 
 @Component({
   standalone: true,
@@ -14,40 +17,56 @@ import AuthService from '@/services/authservice';
 export class LoginPageComponent {
   private router = inject(Router);
   private authService = inject(AuthService);
+  loadingService = inject(LoadingService);
+  notificationService = inject(NotificationService);
   email = '';
   password = '';
-  isLoading = false;
   hidePassword = true;
 
   onSubmit() {
-    this.isLoading = true;
+    this.loadingService.isLoading.set(true);
 
     try {
       this.authService.sanitizeCredentials(this.email, this.password);
     } catch (e: any) {
-      this.isLoading = false;
-      alert(e);
+      this.loadingService.isLoading.set(false);
+
+      if (e instanceof InvalidInputError) {
+        this.notificationService.warning(e.message);
+        return;
+      }
+
+      this.notificationService.error(e.message);
       return;
     }
 
     this.authService.loginUser(this.email, this.password).subscribe({
       next: (res) => {
-        this.isLoading = false;
+        this.loadingService.isLoading.set(false);
 
         if (res.status == 200) {
           if (res.body != null) {
             let token: string = res.body?.token;
             localStorage.setItem('token', token);
-            this.router.navigateByUrl('/home');
+            this.router.navigateByUrl('/home', {
+              replaceUrl: true,
+            });
 
             return;
           }
-          alert('Invalid response');
+          this.notificationService.error('Invalid response');
         }
       },
       error: (err: HttpErrorResponse) => {
-        this.isLoading = false;
-        alert(err.error.error);
+        this.loadingService.isLoading.set(false);
+
+        if (err.error.error) {
+          this.notificationService.error(err.error.error);
+
+          return;
+        }
+
+        this.notificationService.error(`Could not complete request: Code ${err.status}`);
       },
     });
   }
