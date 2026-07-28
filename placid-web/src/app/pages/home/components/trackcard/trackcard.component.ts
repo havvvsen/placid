fe6f78cc2bad397d4ed99643d16960ff0b617c62
Hansen <samhansen.dev@gmail.com>
2026-07-28 17:18:15 +0300

    feat: show if track is currently playing

--- placid-web/src/app/pages/home/components/trackcard/trackcard.component.ts
+++ placid-web/src/app/pages/home/components/trackcard/trackcard.component.ts
@@ -1,18 +1,21 @@
+import PlayerService from '@/services/playerservice';
 import { Track } from '@/shared/models/track';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-track-card-component',
   standalone: true,
   templateUrl: 'trackcard.html',
   host: {
-    class: 'block w-full'
-  }
+    class: 'block w-full',
+  },
 })
 export class TrackCardComponent {
   @Input({ required: true }) track!: Track;
   @Input({ required: true }) bannerServerBaseUrl!: string;
   @Output() play = new EventEmitter<Track>();
+
+  playerService = inject(PlayerService);
 
   onPlay() {
     this.play.emit(this.track);
